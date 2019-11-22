693315d7c725a8342efc475c2478c5ca866b8129
Psage23 <pamelasage3@gmail.com>
2019-11-22 12:21:25 -0700

    background video

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
     <div>
       <div className="App">
       <Navigation/>
+      <video id="background-video" loop autoPlay>
+      <source src="https://player.vimeo.com/external/223082126.sd.mp4?s=baf637d16279940d71f09848ceaa380358065e32&profile_id=164&oauth2_token_id=57447761" type="video/mp4" />
+      <source src="https://player.vimeo.com/external/223082126.sd.mp4?s=baf637d16279940d71f09848ceaa380358065e32&profile_id=164&oauth2_token_id=57447761" type="video/ogg" />
+      Your browser does not support the video tag.
+      </video>
       </div>
       <div className="Body">
       <Route path="/Signup" component={Signup}/>
